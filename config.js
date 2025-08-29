955dc014158d08e83fd8b51c0a7deb9d33bbd28a
peacemakermusicofficial7-star <226865811+peacemakermusicofficial7-star@users.noreply.github.com>
2025-08-29 18:51:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PROVIDE YOUR SESSION ID",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~ywpAhTaC#LNRdUQ4cU7PsTOFAyMsH5O08R5hfffeyr9iWZIGZh6g",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
